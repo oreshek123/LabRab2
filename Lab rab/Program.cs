1e23dce9086ba3aca87d94954bca9e6bd42f41b9
Nastya <anastasya.0702@mail.ru>
2018-06-01 00:29:20 +0600

    Добавьте файлы проекта.

---
+++ Lab rab/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_rab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите путь с файлами");
+            string path = Console.ReadLine();
+            //E:\TNSShort
+            DirectoryInfo dir = new DirectoryInfo(path);
+            List<FileInfo> oldFiles = dir.GetFiles().ToList();
+
+            foreach (FileInfo item in dir.GetFiles())
+            {
+                Console.WriteLine(item.Extension);
+            }
+
+            Console.WriteLine("Выберите одно или несколько расширений");
+            string[] extension = Console.ReadLine().Split(' ');
+
+            List<FileInfo> findedFilesWithExtension = oldFiles.Where(w => extension.Contains(w.Extension.Replace(".", ""))).ToList();
+
+            if (oldFiles.Count != 0)
+                Console.WriteLine($"Общее количество найденных файлов: {oldFiles.Count}");
+            else
+            {
+                Console.WriteLine("Файлы не найдены");
+                return;
+            }
+
+            Console.WriteLine("Разрешить работу над файлами? 1 - да, 0 - нет");
+
+            int.TryParse(Console.ReadLine(), out int choice);
+            if (choice == 1)
+            {
+                foreach (FileInfo item in findedFilesWithExtension)
+                {
+                    if (item.Name.Contains("_"))
+                    {
+                        Console.Write("Заменить \"_\" на ");
+                        string str = item.Name.Replace(".", Console.ReadLine());
+                        str = str.Insert(str.IndexOf($"{item.Extension.Replace(".", "")}"), ".");
+                        File.Move(Path.Combine(dir.FullName, item.Name), Path.Combine(dir.FullName, str));
+                    }
+                    else if (item.Name.Contains("-"))
+                    {
+                        Console.Write("Заменить \"-\" на ");
+                        string str = item.Name.Replace(".", Console.ReadLine());
+                        str = str.Insert(str.IndexOf($"{item.Extension.Replace(".", "")}"), ".");
+                        File.Move(Path.Combine(dir.FullName, item.Name), Path.Combine(dir.FullName, str));
+                    }
+                    else if (item.Name.Contains("."))
+                    {
+                        Console.Write("Заменить \".\" на ");
+                        string str = item.Name.Replace(".", Console.ReadLine());
+                        str = str.Insert(str.IndexOf($"{item.Extension.Replace(".", "")}"), ".");
+                        File.Move(Path.Combine(dir.FullName, item.Name), Path.Combine(dir.FullName, str));
+                    }
+                }
+            }
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
+
---
+++ Lab rab/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("Lab rab")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Lab rab")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("f78bf8ce-f1be-4a2b-804c-ebc24cff28a8")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
